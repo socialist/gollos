cc6531636c7b1f4bacce45d7c160a1f6b958694c
walk <woldman7@gmail.com>
2015-04-07 00:22:04 +0300

    Проверка .gitignore

--- src/config.php
+++ src/config.php
@@ -1,4 +1,4 @@
 <?php
 
-$keyAPI = 'API_KEY';
-$keySecret = 'SECRET_KEY';
+$keyAPI = 'c2dd7f8e-b0c2-45a3-8d6d-47c1414bdfdf';
+$keySecret = 'eafd22a05abd3abd8efb3a19f5d3e0de64bc818ae4da92e4588b9e395ddb1aa0';
